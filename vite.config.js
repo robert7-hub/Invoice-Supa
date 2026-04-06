6cd77da7eb282c0630a6c1ba0866521fe1ec78c2
Robert Ferreira <robert246raf@gmail.com>
2026-04-07 01:53:14 +0200

    Fix: remove external pako modules that caused blank page on deploy

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/Invoice-Supa/',
-  build: {
-    rollupOptions: {
-      external: [
-        'pako/lib/zlib/zstream.js',
-        'pako/lib/zlib/deflate.js',
-        'pako/lib/zlib/inflate.js',
-        'pako/lib/zlib/constants.js',
-      ],
-    },
-  },
   optimizeDeps: {
     include: ['@react-pdf/renderer'],
   },
